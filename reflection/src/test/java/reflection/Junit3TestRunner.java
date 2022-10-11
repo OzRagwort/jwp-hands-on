04b8c9e3d0c4943a48bfde00d35577c9b7a05051
장원영 <32123302+OzRagwort@users.noreply.github.com>
2022-10-11 11:48:03 +0900

    [Reflection 활용하기] 알린(장원영) #3
    feat: reflection 테스트 통과되도록 수정 (#3)

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +12,17 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+        Stream.of(clazz.getDeclaredMethods())
+                .filter(it -> it.getName().startsWith("test"))
+                .forEach(method -> invoke(junit3Test, method));
+    }
+
+    private void invoke(Junit3Test junit3Test, Method method) {
+        try {
+            method.invoke(junit3Test);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.HashSet;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -13,6 +17,12 @@ class ReflectionsTest {
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
 
+        HashSet<Class<?>> classes = new HashSet<>();
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        classes.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        classes.forEach(aClass -> log.info(aClass.getName()));
     }
 }
